7af49cbd62ee3d5466b39399e276fae99221752b
Kai Luo <gluokai@gmail.com>
2017-01-07 11:39:10 +0800

    add udpdump

--- pcap++_test.cc
+++ pcap++_test.cc
@@ -1,7 +1,12 @@
 // Copyright (c) 2017 Kai Luo <gluokai@gmail.com>. All rights reserved.
-#include "pcap++.h"
+#include <chrono>
+#include <thread>
+
 #include "kl/logger.h"
+#include "kl/netdev.h"
 #include "kl/testkit.h"
+#include "kl/udp.h"
+#include "pcap++.h"
 
 class T {};
 
@@ -22,6 +27,42 @@ TEST(T, CompileAndInstall) {
   auto fail_compile =
       sniffer.CompileAndInstall("udp and portrange 50000-65536");
   ASSERT(!fail_compile);
+}
+
+TEST(T, UDPDump) {
+  const std::string message("wtf~imfao~rofl");
+  const char *ifname = "lo";
+  const uint16_t port = 4000;
+  kale::pcap::Sniffer sniffer("lo");
+  auto compile = sniffer.CompileAndInstall(
+      kl::FormatString("udp and port %u", port).c_str());
+  ASSERT(compile);
+  auto send_thread = std::thread([ifname, port, message] {
+    auto addr = kl::netdev::GetAddress(ifname);
+    ASSERT(addr);
+    KL_DEBUG("address %s", (*addr).c_str());
+    auto sock = kl::udp::Socket();
+    ASSERT(sock);
+    kl::env::Defer defer([fd = *sock] { ::close(fd); });
+    auto send = kl::inet::Sendto(*sock, message.c_str(), message.size(), 0,
+                                 (*addr).c_str(), port);
+    KL_DEBUG("packet sent");
+    ASSERT(send);
+  });
+  struct pcap_pkthdr header;
+  KL_DEBUG("waiting for packet");
+  auto start = std::chrono::high_resolution_clock::now();
+  // FIXME(Kai Luo): sniffer.NextPacket costs too much time
+  const uint8_t *packet = sniffer.NextPacket(&header);
+  auto now = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<float> diff = now - start;
+  KL_DEBUG("sniffer.NextPacket costs %fs", diff.count());
+  (void)packet;
+  KL_DEBUG("packet size: %u", header.len);
+  ASSERT(header.len >= message.size());
+  ASSERT(std::string(packet + header.len - message.size(),
+                     packet + header.len) == message);
+  send_thread.join();
 }
 
 int main() { return KL_TEST(); }
