b05081268a21c745f8d251d9ddd8986e57e48f06
Kai Luo <gluokai@gmail.com>
2017-01-07 21:54:32 +0800

    remove libnet++

--- libnet++_test.cc
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2017 Kai Luo <gluokai@gmail.com>. All rights reserved.
-#include "libnet++.h"
-#include "kl/testkit.h"
-
-TEST(kale::libnet::Injector, Constructor, LIBNET_LINK_ADV, "lo") {}
-
-int main() { return KL_TEST(); }
