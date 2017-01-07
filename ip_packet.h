c36885129995a10df65f4b51a37d3524c64762fc
Kai Luo <gluokai@gmail.com>
2017-01-07 23:32:55 +0800

    more comments

--- ip_packet.h
+++ ip_packet.h
@@ -5,16 +5,18 @@
 
 namespace kale {
 namespace ip_packet {
-
 // All these functions won't validate packet.
 bool IsUDP(const uint8_t *packet, size_t len);
 bool IsTCP(const uint8_t *packet, size_t len);
+
 // @addr, @port in network byte order.
 void ChangeDstAddr(uint8_t *packet, size_t len, uint32_t addr);
 void ChangeSrcAddr(uint8_t *packet, size_t len, uint32_t addr);
 void ChangeUDPPort(uint8_t *segment, size_t len, uint16_t port);
 void ChangeTCPPort(uint8_t *segment, size_t len, uint16_t port);
+
 // https://tools.ietf.org/html/rfc791
+// @return: in network byte order
 uint16_t IPHeaderCheckSum(const uint8_t *packet, size_t len);
 uint16_t TCPCheckSum(const uint8_t *segment, size_t len);
 uint16_t UDPCheckSum(const uint8_t *segment, size_t len);
