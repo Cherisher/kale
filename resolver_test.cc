986f8dc15294f21eff13744b14ac5a1738813a33
Kai Luo <gluokai@gmail.com>
2017-01-12 21:03:13 +0800

    add resolver test

---
+++ resolver_test.cc
@@ -0,0 +1,42 @@
+// Copyright (c) 2017 Kai Luo <gluokai@gmail.com>. All rights reserved.
+#include "resolver.h"
+#include "ip_packet.h"
+#include "kl/logger.h"
+#include "kl/testkit.h"
+#include "kl/udp.h"
+
+class T {};
+
+TEST(T, BuildQuery) {
+  auto query = kale::Resolver::BuildQuery("www.google.com", 0x2c13);
+  const char *origin = "\x2c\x13\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03"
+                       "\x77\x77\x77\x06\x67\x6f\x6f\x67\x6c\x65\x03\x63\x6f"
+                       "\x6d\x00\x00\x01\x00\x01";
+  for (size_t i = 0; i < query.size(); ++i) {
+    // KL_DEBUG("%d\t0x%02x 0x%02x", i, origin[i], query[i]);
+    ASSERT(query[i] == static_cast<uint8_t>(origin[i]));
+  }
+}
+
+TEST(T, DNSName) {
+  uint8_t buf[65536];
+  int len;
+  len = kale::ip_packet::BuildNetworkBuffer(buf, sizeof(buf), "bsbsbsb", 0x03,
+                                            "www", 0x06, "google", 0x03, "com",
+                                            0x0);
+  std::string name = kale::Resolver::DNSName("www.google.com");
+  ASSERT(len == static_cast<int>(name.size()));
+  char *buf_str = reinterpret_cast<char *>(buf);
+  ASSERT(name == std::string(buf_str, buf_str + len));
+}
+
+TEST(T, Query) {
+  auto udp_sock = kl::udp::Socket();
+  ASSERT(udp_sock);
+  int fd = *udp_sock;
+  kale::Resolver resovler(fd);
+  auto query = resovler.SendQuery("www.google.com", "8.8.8.8", 53);
+  ASSERT(query);
+}
+
+int main() { return KL_TEST(); }
