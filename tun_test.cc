4957c23877e0cf719c4e7310aee00cbc4718b6ee
Kai Luo <gluokai@gmail.com>
2017-01-13 14:04:20 +0800

    more packets

--- tun_test.cc
+++ tun_test.cc
@@ -32,12 +32,12 @@ TEST(T, Allocation) {
 }
 
 TEST(T, ReadWriteTun) {
+  const int kNumOfPackets = 1 << 10;
   const std::string message("imfao|wtf|rofl~~|rekt");
   const char *ifname = "tun0";
   const char *addr = "10.0.0.10";
-  const char *dst_addr = "123.125.114.144";
   const char *mask = "255.255.255.0";
-  uint16_t port = 4000;
+  const char *dst_addr = "123.125.114.144";
   uint16_t dst_port = 3000;
   auto tun_if = kale::AllocateTun(ifname);
   ASSERT(tun_if);
@@ -48,75 +48,65 @@ TEST(T, ReadWriteTun) {
   ASSERT(kl::netdev::SetAddr(ifname, addr));
   ASSERT(kl::netdev::SetNetMask(ifname, mask));
   ASSERT(kl::netdev::AddRoute(ifname, dst_addr));
-  auto send_thread =
-      std::thread([addr, port, message, ifname, dst_addr, dst_port] {
-        auto sock = kl::udp::Socket();
-        ASSERT(sock);
-        kl::env::Defer defer([fd = *sock] { ::close(fd); });
-        auto bind_if = kl::netdev::BindInterface(*sock, ifname);
-        if (!bind_if) {
-          KL_DEBUG(bind_if.Err().ToCString());
-        }
-        auto send = kl::inet::Sendto(*sock, message.c_str(), message.size(), 0,
-                                     dst_addr, dst_port);
-        ASSERT(send);
-        KL_DEBUG("delivered %d bytes", *send);
-        char buf[65536];
-        int nread = ::read(*sock, buf, sizeof(buf));
-        ASSERT(nread >= 0);
-        KL_DEBUG("read %d bytes", nread);
-        buf[nread] = 0;
-        ASSERT(std::string(buf) == message);
-      });
+  auto send_thread = std::thread([addr, message, ifname, dst_addr, dst_port] {
+    auto sock = kl::udp::Socket();
+    ASSERT(sock);
+    kl::env::Defer defer([fd = *sock] { ::close(fd); });
+    auto bind_if = kl::netdev::BindInterface(*sock, ifname);
+    if (!bind_if) {
+      KL_ERROR(bind_if.Err().ToCString());
+    }
+    for (int i = 0; i < kNumOfPackets; ++i) {
+      auto send = kl::inet::Sendto(*sock, message.c_str(), message.size(), 0,
+                                   dst_addr, dst_port);
+      ASSERT(send);
+      // KL_DEBUG("delivered %d bytes via socket", *send);
+      char buf[65536];
+      int nread = ::read(*sock, buf, sizeof(buf));
+      ASSERT(nread >= 0);
+      // KL_DEBUG("read %d bytes from socket", nread);
+      buf[nread] = 0;
+      ASSERT(std::string(buf) == message);
+    }
+  });
   char buf[65536];
-  KL_DEBUG("waiting for traffic...");
-  int nread = ::read(tun_fd, buf, sizeof(buf));
-  // minimum ip header size + minimum udp header size = 20 + 8 = 28
-  ASSERT(nread >= 28);
-  KL_DEBUG("read %d bytes", nread);
-  // protocol type
-  ASSERT(kale::ip_packet::IsUDP(reinterpret_cast<const uint8_t *>(buf), nread));
-  // header check sum
-  KL_DEBUG("ip header actual checksum: %u",
-           *reinterpret_cast<uint16_t *>(buf + 10));
-  KL_DEBUG("ip header checksum calculated: %u",
-           kale::ip_packet::IPHeaderChecksum(
-               reinterpret_cast<const uint8_t *>(buf), nread));
-  ASSERT(*reinterpret_cast<uint16_t *>(buf + 10) ==
-         kale::ip_packet::IPHeaderChecksum(
-             reinterpret_cast<const uint8_t *>(buf), nread));
-  // udp check sum
-  uint8_t *segment = reinterpret_cast<uint8_t *>(
-      buf + kale::ip_packet::IPHeaderLength(
-                reinterpret_cast<const uint8_t *>(buf), nread));
-  KL_DEBUG("src port %u", ntohs(*reinterpret_cast<const uint16_t *>(segment)));
-  KL_DEBUG("dst port %u",
-           ntohs(*reinterpret_cast<const uint16_t *>(segment + 2)));
-  uint16_t checksum = *reinterpret_cast<const uint16_t *>(segment + 6);
-  KL_DEBUG("udp actual checksum: %u", checksum);
-  KL_DEBUG("udp checksum calculated: %u",
-           kale::ip_packet::UDPChecksum(reinterpret_cast<const uint8_t *>(buf),
-                                        nread));
-  ASSERT(checksum == kale::ip_packet::UDPChecksum(
-                         reinterpret_cast<const uint8_t *>(buf), nread));
-  buf[nread] = '\0';
-  ASSERT(std::string(buf + 28) == message);
-  // swap src/dst
-  uint8_t *packet = reinterpret_cast<uint8_t *>(buf);
-  uint32_t tmp_addr = *reinterpret_cast<uint32_t *>(packet + 12);
-  *reinterpret_cast<uint32_t *>(packet + 12) =
-      *reinterpret_cast<uint32_t *>(packet + 16);
-  *reinterpret_cast<uint32_t *>(packet + 16) = tmp_addr;
-  uint16_t tmp_port = *reinterpret_cast<uint16_t *>(segment);
-  *reinterpret_cast<uint16_t *>(segment) =
-      *reinterpret_cast<uint16_t *>(segment + 2);
-  *reinterpret_cast<uint16_t *>(segment + 2) = tmp_port;
-  kale::ip_packet::UDPFillChecksum(packet, nread);
-  kale::ip_packet::IPFillChecksum(packet, nread);
-  KL_DEBUG("writing to tun");
-  int nwrite = ::write(tun_fd, buf, nread);
-  KL_DEBUG("write %d bytes", nwrite);
-  ASSERT(nwrite == nread);
+  // KL_DEBUG("waiting for traffic...");
+  for (int i = 0; i < kNumOfPackets; ++i) {
+    int nread = ::read(tun_fd, buf, sizeof(buf));
+    // minimum ip header size + minimum udp header size = 20 + 8 = 28
+    ASSERT(nread >= 28);
+    // KL_DEBUG("read %d bytes from tun", nread);
+    uint8_t *packet = reinterpret_cast<uint8_t *>(buf);
+    size_t len = nread;
+    // protocol type
+    ASSERT(kale::ip_packet::IsUDP(packet, len));
+    // header check sum
+    // KL_DEBUG("ip header actual checksum: %u",
+    //          *reinterpret_cast<uint16_t *>(packet + 10));
+    // KL_DEBUG("ip header checksum calculated: %u",
+    //          kale::ip_packet::IPHeaderChecksum(packet, len));
+    ASSERT(*reinterpret_cast<uint16_t *>(buf + 10) ==
+           kale::ip_packet::IPHeaderChecksum(packet, len));
+    // udp check sum
+    uint8_t *segment = kale::ip_packet::SegmentBase(packet, len);
+    // KL_DEBUG("src port %u", kale::ip_packet::UDPSrcPort(packet, len));
+    // KL_DEBUG("dst port %u", kale::ip_packet::UDPDstPort(packet, len));
+    uint16_t checksum = *reinterpret_cast<const uint16_t *>(segment + 6);
+    // KL_DEBUG("udp actual checksum: %u", checksum);
+    // KL_DEBUG("udp checksum calculated: %u",
+    //             kale::ip_packet::UDPChecksum(packet, len));
+    ASSERT(checksum == kale::ip_packet::UDPChecksum(packet, len));
+    buf[nread] = '\0';
+    ASSERT(std::string(buf + 28) == message);
+    // swap src/dst
+    kale::ip_packet::UDPEcho(packet, nread);
+    kale::ip_packet::UDPFillChecksum(packet, nread);
+    kale::ip_packet::IPFillChecksum(packet, nread);
+    // KL_DEBUG("writing to tun");
+    int nwrite = ::write(tun_fd, buf, nread);
+    // KL_DEBUG("write %d bytes back to tun", nwrite);
+    ASSERT(nwrite == nread);
+  }
   send_thread.join();
 }
 
@@ -127,6 +117,18 @@ TEST(T, RawIPv4Socket) {
   }
   ASSERT(raw);
   ::close(*raw);
+}
+
+TEST(T, BindPort) {
+  auto udp = kl::udp::Socket();
+  int fd0 = *udp;
+  kl::env::Defer defer([fd = fd0] { ::close(fd); });
+  udp = kl::udp::Socket();
+  int fd1 = *udp;
+  defer([fd = fd1] { ::close(fd); });
+  uint16_t port = 40000;
+  ASSERT(kl::inet::Bind(fd0, "0.0.0.0", port));
+  ASSERT(!kl::inet::Bind(fd1, "127.0.0.1", port));
 }
 
 int main() { return KL_TEST(); }
