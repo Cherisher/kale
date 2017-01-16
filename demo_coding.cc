4f96c7a4a4c8d5469bcc6714ee90848d717a4904
Kai Luo <gluokai@gmail.com>
2017-01-16 15:09:14 +0800

    remove compression

--- demo_coding.cc
+++ demo_coding.cc
@@ -17,22 +17,11 @@ kale::arcfour::Cipher cipher(kKey, sizeof(kKey));
 
 void Encode(const uint8_t *buffer, size_t len, std::vector<uint8_t> *encode) {
   *encode = cipher.Encrypt(buffer, len);
-  std::string compress;
-  snappy::Compress(reinterpret_cast<const char *>(encode->data()),
-                   encode->size(), &compress);
-  *encode = std::vector<uint8_t>(compress.begin(), compress.end());
 }
 
 kl::Status Decode(const uint8_t *buffer, size_t len,
                   std::vector<uint8_t> *decode) {
-  std::string uncompress;
-  bool ok = snappy::Uncompress(reinterpret_cast<const char *>(buffer), len,
-                               &uncompress);
-  if (!ok) {
-    return kl::Err("failed to uncompress from buffer");
-  }
-  *decode = cipher.Decrypt(reinterpret_cast<const uint8_t *>(uncompress.data()),
-                           uncompress.size());
+  *decode = cipher.Decrypt(buffer, len);
   return kl::Ok();
 }
 
