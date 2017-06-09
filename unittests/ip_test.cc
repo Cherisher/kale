630505d85b5128497acdd319f8b88bc8531bc367
Kai Luo <gluokai@gmail.com>
2017-06-09 20:38:19 +0800

    Add test for checksum

--- unittests/ip_test.cc
+++ unittests/ip_test.cc
@@ -43,12 +43,24 @@ TEST(T, BuildNetworkBuffer1) {
   ASSERT(len == static_cast<int>(::strlen(message)));
   ASSERT(std::string(reinterpret_cast<char *>(packet)) == message);
   len = kale::ip::BuildNetworkBuffer(buf, sizeof(buf), "sqwb", message,
-                                            0xaabbccdd, 0xff00, 0x11);
+                                     0xaabbccdd, 0xff00, 0x11);
   ASSERT(len == 1 + 2 + 4 + static_cast<int>(::strlen(message)));
   len = kale::ip::BuildNetworkBuffer(buf, sizeof(buf), "#s", 0, message);
   ASSERT(len == 0);
   len = kale::ip::BuildNetworkBuffer(buf, sizeof(buf), "#s", 1, message);
   ASSERT(len == 1);
+}
+
+TEST(T, CheckSum) {
+  const uint8_t packet[] = {
+      0x45, 0x00, 0x00, 0x34, 0x9d, 0x8a, 0x40, 0x00, 0x40, 0x06, 0xe1,
+      0x74, 0x0a, 0x00, 0x00, 0x01, 0x4a, 0x7d, 0x67, 0x47, 0x90, 0x10,
+      0x01, 0xbb, 0x44, 0xc6, 0xc0, 0x30, 0x61, 0x4e, 0x74, 0xcd, 0x80,
+      0x10, 0x58, 0x64, 0xff, 0xff, 0x00, 0x00, 0x01, 0x01, 0x08, 0x0a,
+      0x00, 0x3e, 0x27, 0xdb, 0x96, 0xa5, 0x36, 0xf7,
+  };
+  uint16_t checksum = kale::ip::TCPChecksum(packet, sizeof(packet));
+  ASSERT(checksum = 65535);
 }
 
 }  // namespace
